389c2fb7f5bcdaf4eae919a2a12324a4c94e9f11
trustytrojan <tarbot24@hotmail.com>
2022-12-13 12:08:23 -0500

    ok?

--- classes/managers/BaseManager.ts
+++ classes/managers/BaseManager.ts
@@ -1,9 +1,10 @@
 import BetterMap from '../../BetterMap';
+import Base from '../Base';
 import Client from '../Client';
 
 export default class BaseManager {
   protected readonly client: Client;
-  readonly cache: BetterMap<string, any>;
+  readonly cache: BetterMap<string, Base>;
 
   constructor(client) {
     Object.defineProperty(this, 'client', { value: client });
