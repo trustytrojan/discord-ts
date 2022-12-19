68b10dc40b6403deecb33635549e785f818d819e
trustytrojan <tarbot24@hotmail.com>
2022-12-18 22:30:01 -0500

    push

--- discord.ts/managers/BaseManager.ts
+++ discord.ts/managers/BaseManager.ts
@@ -1,9 +1,13 @@
 import Client from '../Client';
+import Base from '../structures/Base';
+import BetterMap from '../util/BetterMap';
 
 export default abstract class BaseManager {
   protected readonly client: Client;
+  protected readonly cache: BetterMap<string, Base>;
 
   constructor(client) {
     Object.defineProperty(this, 'client', { value: client });
+    this.cache = new BetterMap<string, Base>();
   }
 }
